01ab6ad9f9da75eb0596ade2db4c5c52dfb3334a
Hamza Sharif <112539879+Hamzasharif6666@users.noreply.github.com>
2023-06-30 02:36:20 -0600

    1 month upgrade
    :+1:

---
+++ script.js
@@ -0,0 +1,50 @@
+
+const name = document.querySelector("#name"),
+  subject = document.querySelector("#subject"),
+  email = document.querySelector("#email"),
+  message = document.querySelector("#message");
+
+function sendEmail() {
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    From: email.value,
+    Subject: subject.value,
+    Body: message.value,
+  }).then((message) => alert(message));
+
+  console.log("Here is the info", subject.value, email.value, message.value);
+}
+
+// Credits https://tobiasahlin.com/moving-letters/#7
+anime
+  .timeline({ loop: false })
+  .add({
+    targets: ".intro-title .letter",
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 1100,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i,
+  })
+  .add({
+    targets: ".ml7",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
+
+
+// CopyToClipboard
+  function copyToClipboard(text) {
+    const tempElement = document.createElement('textarea');
+    tempElement.value = text;
+    document.body.appendChild(tempElement);
+    tempElement.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempElement);
+    alert('Nickname copied to clipboard!');
+  }
+//
