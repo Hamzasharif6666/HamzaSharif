eaa6f9281391b52ae98e4c63fb064ece83e80bb1
Hamza Sharif <112539879+Hamzasharif6666@users.noreply.github.com>
2023-09-07 21:01:01 -0600

    change website design
    big update

--- script.js
+++ script.js
@@ -37,15 +37,52 @@ anime
   });
 
 
-// CopyToClipboard Nicknames.
+  document.addEventListener("DOMContentLoaded", function () {
+    const nicknameElements = document.querySelectorAll(".nickname");
+  
+    nicknameElements.forEach(function (element) {
+      element.addEventListener("click", function () {
+        const isCopied = element.getAttribute("data-copied") === "true";
+        if (!isCopied) {
+          const text = element.textContent.trim();
+          copyToClipboard(text);
+          element.setAttribute("data-copied", "true");
+          element.classList.add("copied");
+  
+          // Toggle the copy status text to "Coppied"
+          const copyStatus = element.querySelector(".copy-status");
+          if (copyStatus) {
+            copyStatus.textContent = "Coppied";
+            copyStatus.classList.add("fade-in"); // Apply the fade-in class
+          }
+  
+          setTimeout(() => {
+            element.setAttribute("data-copied", "false");
+            element.classList.remove("copied");
+  
+            // Reset the copy status text to "Copy" after fading in
+            if (copyStatus) {
+              copyStatus.textContent = "Copy";
+              copyStatus.classList.remove("fade-in"); // Remove the fade-in class
+            }
+          }, 1500); // Reset to "Copy" after 1.5 seconds
+        }
+      });
+    });
+  });
+  
   function copyToClipboard(text) {
-    const tempElement = document.createElement('textarea');
-    tempElement.value = text;
-    document.body.appendChild(tempElement);
-    tempElement.select();
-    document.execCommand('copy');
-    document.body.removeChild(tempElement);
-    alert('Nickname copied to clipboard!');
+    const tempInput = document.createElement("input");
+    tempInput.value = text;
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
   }
   
-//
+  
+  
+  
+  
+  
+  
